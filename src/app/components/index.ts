d0ce48b3204b01d19aa721d9f6cb5169169c5130
Hahahayabanan <oleg.volobuev@fullstack-development.com>
2020-07-22 17:25:07 +0400

    Remove unused app/components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,2 @@
-export { Header } from './Header/Header';
-export { PageNavigation } from './PageNavigation/PageNavigation';
-export { WithAccount } from './WithAccount/WithAccount';
-export { MainLayout } from './MainLayout';
-export { Content } from './Content';
+export { MainLayout } from "./MainLayout";
+export { Content } from "./Content";
