03cba77c2817e59fe76f2ef9615fbcf549d7b545
Hahahayabanan <oleg.volobuev@fullstack-development.com>
2020-07-22 14:08:54 +0400

    Clear routes tree

--- src/app/components/Content/Content.tsx
+++ src/app/components/Content/Content.tsx
@@ -1,50 +1,36 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
 
-import { routes } from '../../routes';
-import * as pages from '../../pages';
+import { routes } from "../../routes";
+import * as pages from "../../pages";
 
 export const Content: React.FC = () => {
   return (
     <Switch>
-      {process.env.NODE_ENV !== 'production' && (
-        <Route exact path={routes.demo.getRoutePath()} component={pages.DemoPage} />
+      {process.env.NODE_ENV !== "production" && (
+        <Route
+          exact
+          path={routes.demo.getRoutePath()}
+          component={pages.DemoPage}
+        />
       )}
-      <Route path={routes.account.getRoutePath()} component={pages.AccountPage} />
-      <Route exact path={routes.lend.getRoutePath()} component={pages.Lend} />
       <Route
-        exact
-        path={routes.borrow.getRoutePath()}
-        component={makeUnimplementedComponent('Borrow')}
+        path={routes.summary.getRoutePath()}
+        component={pages.SummaryPage}
       />
       <Route
         exact
-        path={routes.liquidations.getRoutePath()}
-        component={makeUnimplementedComponent('Liquidations')}
+        path={routes.savings.getRoutePath()}
+        component={makeUnimplementedComponent("Savings")}
       />
-      <Route path={routes.history.getRoutePath()} component={pages.HistoryPage} />
-      <Route
-        exact
-        path={routes.governance.getRoutePath()}
-        component={makeUnimplementedComponent('Governance')}
-      />
-      <Route exact path={routes.stats.getRoutePath()} component={pages.StatsPage} />
-      <Route
-        exact
-        path={routes['privacy-policy'].getRoutePath()}
-        component={makeUnimplementedComponent('Privacy policy')}
-      />
-      <Route
-        exact
-        path={routes['terms-of-service'].getRoutePath()}
-        component={makeUnimplementedComponent('Terms of service')}
-      />
-      <Route exact path={routes.strategies.getRoutePath()} component={pages.StrategiesPage} />
+
       <Redirect to="/" />
     </Switch>
   );
 };
 
 function makeUnimplementedComponent(componentLabel: string) {
-  return () => <div style={{ fontSize: 45 }}>{`${componentLabel} not implemented`}</div>;
+  return () => (
+    <div style={{ fontSize: 45 }}>{`${componentLabel} not implemented`}</div>
+  );
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,31 +1,9 @@
-import build from 'build-route-tree';
+import build from "build-route-tree";
 
 const rawTree = {
   demo: null,
-  account: {
-    summary: null,
-    stakes: null,
-    borrows: null,
-  },
-  balance: null,
-  pool: null,
-  stats: null,
-  proposals: null,
-  distributions: null,
-  lend: null,
-  borrow: null,
-  sell: null,
-  strategies: null,
-  history: {
-    transaction: null,
-    profit: null,
-    liquidations: null,
-  },
-  bounty: null,
-  liquidations: null,
-  governance: null,
-  'privacy-policy': null,
-  'terms-of-service': null,
+  summary: null,
+  savings: null,
 };
 
 export const routes = build(rawTree);
