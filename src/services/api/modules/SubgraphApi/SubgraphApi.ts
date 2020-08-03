ea01fa8be225bb5d3f053826b36b0d4b5f1737ac
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-03 17:57:20 +0300

    Correct subgraph queries

--- src/services/api/modules/SubgraphApi/SubgraphApi.ts
+++ src/services/api/modules/SubgraphApi/SubgraphApi.ts
@@ -1,8 +1,7 @@
-import { Observable, timer, combineLatest } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import ApolloClient from 'apollo-client';
 import * as R from 'ramda';
-import { map, switchMap } from 'rxjs/operators';
-import BN from 'bn.js';
+import { map } from 'rxjs/operators';
 
 import { memoize } from 'utils/decorators';
 import { SavingsPool } from 'model/types';
@@ -10,9 +9,6 @@ import { SavingsPool } from 'model/types';
 import { makeSubgraphApi } from './makeSubgraphApi';
 import { convertUser, convertSavingsPool } from './converters';
 import { SubgraphConfig } from './model';
-
-const RELOAD_APY_HISTORY_TIMEOUT = 1 * 60 * 60 * 1000;
-const APY_HISTORY_TIMEOUT = 3 * 24 * 60 * 60 * 1000;
 
 export class SubgraphApi {
   private sdk = makeSubgraphApi(this.apolloClient);
@@ -34,7 +30,6 @@ export class SubgraphApi {
     return this.sdk
       .User({
         id: address.toLowerCase(),
-        savingsPoolsAPYsDateGT: new BN(Date.now()).toString(),
       })
       .pipe(
         map(({ user }) => {
@@ -45,49 +40,34 @@ export class SubgraphApi {
 
   @memoize(R.identity)
   public loadUserSavingsPools$(address: string): Observable<SavingsPool[]> {
-    return timer(0, RELOAD_APY_HISTORY_TIMEOUT).pipe(
-      switchMap(() =>
-        combineLatest([
-          this.loadSubgraphConfig$(),
-          this.sdk.User({ id: address.toLowerCase(), savingsPoolsAPYsDateGT: getAPYsDateGT() }),
-        ]).pipe(
-          map(([{ aprDecimals }, { user }]) => {
-            return user ? user.savingsPools.map(pool => convertSavingsPool(pool, aprDecimals)) : [];
-          }),
-        ),
-      ),
+    return combineLatest([
+      this.loadSubgraphConfig$(),
+      this.sdk.User({ id: address.toLowerCase() }),
+    ]).pipe(
+      map(([{ aprDecimals }, { user }]) => {
+        return user ? user.savingsPools.map(pool => convertSavingsPool(pool, aprDecimals)) : [];
+      }),
     );
   }
 
   @memoize(R.identity)
   public loadSavingsPool$(address: string): Observable<SavingsPool | null> {
-    return timer(0, RELOAD_APY_HISTORY_TIMEOUT).pipe(
-      switchMap(() =>
-        combineLatest([
-          this.loadSubgraphConfig$(),
-          this.sdk.SavingsPool({ id: address.toLowerCase(), apysDateGT: getAPYsDateGT() }),
-        ]).pipe(
-          map(([{ aprDecimals }, { savingsPool }]) => {
-            return savingsPool ? convertSavingsPool(savingsPool, aprDecimals) : null;
-          }),
-        ),
-      ),
+    return combineLatest([
+      this.loadSubgraphConfig$(),
+      this.sdk.SavingsPool({ id: address.toLowerCase() }),
+    ]).pipe(
+      map(([{ aprDecimals }, { savingsPool }]) => {
+        return savingsPool ? convertSavingsPool(savingsPool, aprDecimals) : null;
+      }),
     );
   }
 
   @memoize()
   public loadSavingsPools$(): Observable<SavingsPool[]> {
-    return timer(0, RELOAD_APY_HISTORY_TIMEOUT).pipe(
-      switchMap(() =>
-        combineLatest([
-          this.loadSubgraphConfig$(),
-          this.sdk.SavingsPools({ apysDateGT: getAPYsDateGT() }),
-        ]).pipe(
-          map(([{ aprDecimals }, { savingsPools }]) => {
-            return savingsPools.map(pool => convertSavingsPool(pool, aprDecimals));
-          }),
-        ),
-      ),
+    return combineLatest([this.loadSubgraphConfig$(), this.sdk.SavingsPools()]).pipe(
+      map(([{ aprDecimals }, { savingsPools }]) => {
+        return savingsPools.map(pool => convertSavingsPool(pool, aprDecimals));
+      }),
     );
   }
 
@@ -100,8 +80,4 @@ export class SubgraphApi {
       }),
     );
   }
-}
-
-function getAPYsDateGT(): string {
-  return new BN(Date.now()).sub(new BN(APY_HISTORY_TIMEOUT)).divn(1000).toString();
 }
