1ffd6a9ad90d2b9f2247c7e2e5a46a8b29729b52
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-12 16:25:32 +0300

    Update privacy policy

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,2 +1,2 @@
-export { default as PRIVACY_POLICY_URL } from './Akropolis_Platform_Privacy_Policy.pdf';
+export { default as PRIVACY_POLICY_URL } from './Akropolis_Platform_Privacy_Policy_08_11_2020.pdf';
 export { default as T_AND_C_URL } from './Sparta_Platform_Terms_of_Service.pdf';
