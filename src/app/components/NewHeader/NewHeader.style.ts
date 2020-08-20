1ac7ed35986b73b7cc4e1db4a129dca2ec448ad6
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-20 17:58:50 +0300

    Minor style fixes

--- src/app/components/NewHeader/NewHeader.style.ts
+++ src/app/components/NewHeader/NewHeader.style.ts
@@ -3,7 +3,8 @@ import { makeStyles } from 'utils/styles';
 export const useStyles = makeStyles(
   {
     root: {
-      padding: '16px 50px',
+      padding: '10px 50px',
+      minHeight: 72,
       display: 'flex',
       justifyContent: 'space-between',
     },
