ae6a33ac38b15877c94bd9ce5452294dea300ce3
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-08-21 18:24:41 +0300

    Add TUSD mainnet address

--- src/env.ts
+++ src/env.ts
@@ -86,7 +86,7 @@ const ethNetworkConfigsForMainnet: INetworkConfig = {
     DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
     USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     USDT: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-    TUSD: testnetTokens.TUSD,
+    TUSD: '0x0000000000085d4780b73119b644ae5ecd22b376',
     sUSD: '0x57ab1ec28d129707052df4df418d58a2d46d5f51',
     renBTC: testnetTokens.renBTC,
     WBTC: testnetTokens.WBTC,
