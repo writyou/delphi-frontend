fc92c6b53577ade055d4ca94f64545c4ad076acc
Geksanit <alserpet@e-mail.ru>
2020-08-28 16:16:29 +0700

    fix rewards transaction amount filtering

--- src/services/api/modules/RewardsApi.ts
+++ src/services/api/modules/RewardsApi.ts
@@ -17,7 +17,9 @@ export class RewardsApi {
 
   @memoize(R.identity)
   public getUserRewards$(userAddress: string) {
-    return this.savings.getUserRewards$(userAddress);
+    return this.savings
+      .getUserRewards$(userAddress)
+      .pipe(map(rewards => rewards.filter(a => !a.isZero())));
   }
 
   @autobind
@@ -28,7 +30,6 @@ export class RewardsApi {
   @memoize(R.identity)
   public getUserTotalRewardsBalance(userAddress: string): Observable<LiquidityAmount> {
     return this.getUserRewards$(userAddress).pipe(
-      map(rewards => rewards.filter(a => !a.isZero())),
       switchMap(rewards =>
         rewards.length
           ? combineLatest(rewards.map(a => this.prices.getTokenPrice$(a.currency.address))).pipe(
@@ -48,7 +49,6 @@ export class RewardsApi {
   @memoize(R.identity)
   public getUserRewardsData$(userAddress: string) {
     return this.getUserRewards$(userAddress).pipe(
-      map(rewards => rewards.filter(a => !a.isZero())),
       switchMap(rewards =>
         rewards.length
           ? combineLatest(rewards.map(a => this.prices.getTokenPrice$(a.currency.address))).pipe(
